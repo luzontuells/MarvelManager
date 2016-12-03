144fdb1a45b5e17ba5cd1561bbf6580fa7381242
luzontuells@gmail.com <project103>
2016-12-03 02:41:08 +0100

    Third Commit

--- app/src/main/java/luzontuells/marvelmanager/activities/FirstActivity.java
+++ app/src/main/java/luzontuells/marvelmanager/activities/FirstActivity.java
@@ -1,9 +1,7 @@
 package luzontuells.marvelmanager.activities;
 
 import android.content.Context;
-import android.content.Intent;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -15,9 +13,23 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
+
 import luzontuells.marvelmanager.R;
 import luzontuells.marvelmanager.data.Item;
 
@@ -26,6 +38,12 @@ public class FirstActivity extends AppCompatActivity implements AdapterView.OnIt
 
 
     private static final String TAG_FIRST_ACTIVITY = "First Activity";
+    private static final String JSON_URL = "http://gateway.marvel.com/v1/public/characters?ts=1&apikey=94f4341859283f334a8e1316d7b12e42&hash=aca24562b84ef49172856f5e28d1f95a";
+
+    private String[] mListNames;
+    private String[] mListDescriptors;
+    private String[] mListImages;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,28 +53,31 @@ public class FirstActivity extends AppCompatActivity implements AdapterView.OnIt
         final ListView mListView = (ListView) this.findViewById(R.id.list_view_first);
         mListView.setOnItemClickListener(this);
 
-        // This 'String[]' data could be retrieved from a file, for instance
-        String[] mListValues = {"test"};
+        JSONObject json = null;
+        try {
+            json = new MyAlternativeThread().execute(JSON_URL).get();
+            JSONObject data = json.getJSONObject("data");
+            JSONArray results = data.getJSONArray("results");
+//                    Log.e(FirstActivity.TAG_FIRST_ACTIVITY, "We obtain: "+json2.get("results"));
+            ArrayList<Item> mListArray = new ArrayList<>();
+            String[] names = null, images = null, descriptions = null;
+            for (int i = 0; i < results.length(); i++) {
+                JSONObject c = results.getJSONObject(i);
 
-        //TODO: Obtain Name values from Marvel API
-
-        // Using a custom adapter for the 'ListView' -----------------------------------------------
-        // 'ArrayList' with image references (DO NOT INCLUDE FILE EXTENSIONS!)
-        String[] mListImages = {"androidicon"};
-
-        //TODO: Obtain Image Values from Marvel API
+                //TODO: De las URL a Imágenes!
+                mListArray.add(new Item(c.getJSONObject("thumbnail").getString("path"),
+                        c.getString("name"), c.getString("description")));
+            }
+            mListView.setAdapter(new MyListAdapter(this, 0, mListArray));
 
 
-        String[] mListBodies = {"androidicon"};
-
-        //TODO: Obtain Body Values from Marvel API
-
-        ArrayList<Item> mListArray = new ArrayList<>();
-        // Just populating the 'ArrayList' with the 'Item[]'
-        for (int idx = 0; idx < mListValues.length; idx++)
-            mListArray.add(new Item(mListImages[idx], mListValues[idx], mListBodies[idx]));
-
-        mListView.setAdapter(new MyListAdapter(this, 0, mListArray));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
     }
 
 
@@ -66,23 +87,24 @@ public class FirstActivity extends AppCompatActivity implements AdapterView.OnIt
 //        Intent intent = new Intent(this,SecondActivity.class);
 //        startActivity(intent);
 
-        //    {
-        // 'ConnectivityManager' answers queries about the state of network connectivity
-        ConnectivityManager mConnectionManager = (ConnectivityManager) this.getSystemService(Context.CONNECTIVITY_SERVICE);
-        // 'NetworkInfo' describes the status of a network interface
-        NetworkInfo mNetworkInfo = mConnectionManager.getActiveNetworkInfo();
-        Log.i(FirstActivity.TAG_FIRST_ACTIVITY, mNetworkInfo.getTypeName() + " " + mNetworkInfo.getState().toString());
 
-        // 'isConnected()' handles cases like flaky mobile networks, airplane mode, and restricted background data
-        if (mNetworkInfo != null && mNetworkInfo.isConnected())
-        {
-
-        }
-        else
-        {
-            Log.i(FirstActivity.TAG_FIRST_ACTIVITY, "Connection  not available");
-            Toast.makeText(this, "Connection  not available", Toast.LENGTH_SHORT).show();
-        }
+//        //    {
+//        // 'ConnectivityManager' answers queries about the state of network connectivity
+//        ConnectivityManager mConnectionManager = (ConnectivityManager) this.getSystemService(Context.CONNECTIVITY_SERVICE);
+//        // 'NetworkInfo' describes the status of a network interface
+//        NetworkInfo mNetworkInfo = mConnectionManager.getActiveNetworkInfo();
+//        Log.i(FirstActivity.TAG_FIRST_ACTIVITY, mNetworkInfo.getTypeName() + " " + mNetworkInfo.getState().toString());
+//
+//        // 'isConnected()' handles cases like flaky mobile networks, airplane mode, and restricted background data
+//        if (mNetworkInfo != null && mNetworkInfo.isConnected())
+//        {
+//
+//        }
+//        else
+//        {
+//            Log.i(FirstActivity.TAG_FIRST_ACTIVITY, "Connection  not available");
+//            Toast.makeText(this, "Connection  not available", Toast.LENGTH_SHORT).show();
+//        }
     }
 
 
@@ -133,167 +155,65 @@ public class FirstActivity extends AppCompatActivity implements AdapterView.OnIt
             return convertView;
         }
     }
+
+    private class MyAlternativeThread extends AsyncTask<String, Void, JSONObject> {
+
+        @Override
+        protected JSONObject doInBackground(String[] params) {
+            try {
+                // The input arguments are fetched in order
+                URL myUrl = new URL(params[0]);   // Throws 'MalformedURLException'
+                HttpURLConnection myConnection = (HttpURLConnection) myUrl.openConnection();   // Throws 'IOException'
+                myConnection.setRequestMethod("GET");
+                myConnection.setDoInput(true);
+                // Starting the query
+                myConnection.connect();   // Throws 'IOException'
+                int respCode = myConnection.getResponseCode();   // Throws 'IOException'
+                Log.e(FirstActivity.TAG_FIRST_ACTIVITY, "The response is: " + respCode);
+                if (respCode == HttpURLConnection.HTTP_OK)
+                    Log.e(FirstActivity.TAG_FIRST_ACTIVITY, "HTTP_OK");
+                {
+                    JSONObject json = JSONReader.readJsonFromUrl(JSON_URL);
+                    return json;
+                }
+            } catch (MalformedURLException e1) {
+                e1.printStackTrace();
+            } catch (IOException e2) {
+                e2.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(JSONObject json) {
+            super.onPostExecute(json);
+        }
+    }
+
+    public static class JSONReader {
+
+        private static String readAll(Reader rd) throws IOException {
+            StringBuilder sb = new StringBuilder();
+            int cp;
+            while ((cp = rd.read()) != -1) {
+                sb.append((char) cp);
+            }
+            return sb.toString();
+        }
+
+        public static JSONObject readJsonFromUrl(String url) throws IOException, JSONException {
+            InputStream is = new URL(url).openStream();
+            try {
+                BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
+                String jsonText = readAll(rd);
+                JSONObject json = new JSONObject(jsonText);
+                return json;
+            } finally {
+                is.close();
+            }
+        }
+    }
 }
-
-
-
-//    @Override
-//    public void onClick(View whichView)
-//    {
-//        // 'ConnectivityManager' answers queries about the state of network connectivity
-//        ConnectivityManager mConnectionManager = (ConnectivityManager) this.getSystemService(Context.CONNECTIVITY_SERVICE);
-//        // 'NetworkInfo' describes the status of a network interface
-//        NetworkInfo mNetworkInfo = mConnectionManager.getActiveNetworkInfo();
-//        Log.i(MainActivity.TAG_FIRST_ACTIVITY, mNetworkInfo.getTypeName() + " " + mNetworkInfo.getState().toString());
-//
-//        // 'isConnected()' handles cases like flaky mobile networks, airplane mode, and restricted background data
-//        if (mNetworkInfo != null && mNetworkInfo.isConnected())
-//        {
-//            if (whichView.getId() == R.id.imgBtnDownload)
-//            {
-//                // Do whatever you need
-//                final String urlString = "http://www.tutorialspoint.com/green/images/logo.png";
-//                // Querying the 'URL' from an alternative thread
-//                new MyAlternativeThread().execute(urlString);
-//            }
-//            else if (whichView.getId() == R.id.btnReadFeed)
-//            {
-//                // The feed to be accessed
-//                final String urlFeed = "http://www.theguardian.com/international/rss";
-//                //final String urlFeed = "http://www.revistalatahona.com/feed";
-//                // Querying the feed 'URL' from an alternative thread
-//                new MyParsingThread(this).execute(urlFeed);
-//            }
-//        }
-//        else
-//        {
-//            Log.i(MainActivity.TAG_FIRST_ACTIVITY, "Connection  not available");
-//            Toast.makeText(this, "Connection  not available", Toast.LENGTH_SHORT).show();
-//        }
-//    }
-//
-//// AsyncTask<TypeOfVarArgParams, ProgressValue, ResultValue>
-//private class MyAlternativeThread extends AsyncTask<String, Void, Bitmap>
-//{
-//    @Override
-//    protected Bitmap doInBackground(String[] params)
-//    {
-//        try
-//        {
-//            // The input arguments are fetched in order
-//            URL myUrl = new URL(params[0]);   // Throws 'MalformedURLException'
-//            HttpURLConnection myConnection = (HttpURLConnection) myUrl.openConnection();   // Throws 'IOException'
-//            myConnection.setRequestMethod("GET");
-//            myConnection.setDoInput(true);
-//            // Starting the query
-//            myConnection.connect();   // Throws 'IOException'
-//            int respCode = myConnection.getResponseCode();   // Throws 'IOException'
-//            Log.i(MainActivity.TAG_FIRST_ACTIVITY, "The response is: " + respCode);
-//            if (respCode == HttpURLConnection.HTTP_OK)
-//            {
-//                InputStream myInStream = myConnection.getInputStream();   // Throws 'IOException'
-//
-//                Bitmap myBitmap = BitmapFactory.decodeStream(myInStream);
-//
-//                myInStream.close();   // Always close the 'InputStream'
-//
-//                return myBitmap;
-//            }
-//        }
-//        catch (MalformedURLException e1) { e1.printStackTrace(); }
-//        catch (IOException e2) { e2.printStackTrace(); }
-//
-//        return null;
-//    }
-//
-//    @Override
-//    protected void onPostExecute(Bitmap bitmap)
-//    {
-//        super.onPostExecute(bitmap);
-//
-//        if (bitmap != null)
-//            mImageView.setImageBitmap(bitmap);
-//        else
-//            Log.i(MainActivity.TAG_FIRST_ACTIVITY, "No image to be loaded");
-//    }
-//}
-//
-//private class MyParsingThread extends AsyncTask<String, Void, String>
-//{
-//    private Context threadContext;
-//
-//    public MyParsingThread(Context mContext) { this.threadContext = mContext; }
-//
-//    @Override
-//    protected String doInBackground(String[] params)
-//    {
-//        try
-//        {
-//            // The input arguments are fetched in order
-//            URL myUrl = new URL(params[0]);   // Throws 'MalformedURLException'
-//            HttpURLConnection myConnection = (HttpURLConnection) myUrl.openConnection();   // Throws 'IOException'
-//            myConnection.setRequestMethod("GET");
-//            myConnection.setDoInput(true);
-//            // Starting the query
-//            myConnection.connect();   // Throws 'IOException'
-//            int respCode = myConnection.getResponseCode();   // Throws 'IOException'
-//            Log.i(MainActivity.TAG_FIRST_ACTIVITY, "The response is: " + respCode);
-//            if (respCode == HttpURLConnection.HTTP_OK)
-//            {
-//                InputStream myInStream = myConnection.getInputStream();   // Throws 'IOException'
-//
-//                XmlPullParser myXmlParser = Xml.newPullParser();
-//                //myXmlParser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);   // Throws 'XmlPullParserException'
-//                myXmlParser.setInput(myInStream, null);   // Throws 'XmlPullParserException'
-//
-//                String fullTitleString = "";
-//                int event = myXmlParser.nextTag();
-//                // This parsing process is ONLY ensured to be valid for the specific feed accessed
-//                while (myXmlParser.getEventType() != XmlPullParser.END_DOCUMENT)
-//                {
-//                    switch (event)
-//                    {
-//                        case XmlPullParser.START_TAG:
-//                            if (myXmlParser.getName().equals("item"))
-//                            {
-//                                myXmlParser.nextTag();
-//                                myXmlParser.next();
-//                                fullTitleString += "- " + myXmlParser.getText() + "\n";
-//                            }
-//                            break;
-//                    }
-//
-//                    event = myXmlParser.next();
-//                }
-//                Log.i(MainActivity.TAG_FIRST_ACTIVITY, fullTitleString);
-//
-//                myInStream.close();   // Always close the 'InputStream'
-//
-//                return fullTitleString;
-//            }
-//        }
-//        catch (MalformedURLException e1) { e1.printStackTrace(); }
-//        catch (IOException e2) { e2.printStackTrace(); }
-//        catch (XmlPullParserException e3) { e3.printStackTrace(); }
-//
-//        return null;
-//    }
-//
-//    @Override
-//    protected void onPostExecute(String inString)
-//    {
-//        super.onPostExecute(inString);
-//
-//        if (inString != null)
-//            Toast.makeText(this.threadContext, inString, Toast.LENGTH_LONG).show();
-//        else
-//            Log.i(MainActivity.TAG_FIRST_ACTIVITY, "No feed to be loaded");
-//    }
-//}
-
-
-
-
-
-
-
